d8c3cad77a9df687ffd2cfea3fea643e6fe66fe4
Swap-07 <rathodswapnil1135@gmail.com>
2022-07-17 13:59:23 +0530

    my first commit

---
+++ blogproject/blogproject/urls.py
@@ -0,0 +1,26 @@
+"""blogproject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,re_path
+from myApp.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path('^$',post_list_view),
+    re_path('(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)',post_detail_view,name="post_detail"),
+    re_path('(?P<id>\d+)/share',mail_send_view),
+    re_path('tags/(?P<tag_slug>[-\w]+)/$',post_list_view,name="post_list_tag_name")
+]
